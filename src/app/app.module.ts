9b0faa70940647a8f58b386fe3944a0f5899672b
Tiago Yamamoto <japa@rede5.com.br>
2021-07-04 20:29:04 -0300

    88. Ingresar Presupuesto TS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Modulos
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
+// Components
 import { AppComponent } from './app.component';
 import { IngresarPresupuestoComponent } from './components/ingresar-presupuesto/ingresar-presupuesto.component';
 import { IngresarGastoComponent } from './components/gastos/ingresar-gasto/ingresar-gasto.component';
 import { ListarGastoComponent } from './components/gastos/listar-gasto/listar-gasto.component';
 import { GastosComponent } from './components/gastos/gastos.component';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +23,8 @@ import { GastosComponent } from './components/gastos/gastos.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
