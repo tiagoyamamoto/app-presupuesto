dde6d24bc8dd92a09a6f79b6a543e0a7a5fe492e
Tiago Yamamoto <japa@rede5.com.br>
2021-07-05 18:17:12 -0300

    94. Listado de Gastos HTML

--- src/app/components/gastos/listar-gasto/listar-gasto.component.ts
+++ src/app/components/gastos/listar-gasto/listar-gasto.component.ts
@@ -1,21 +1,35 @@
 import { PresupuestoService } from 'src/app/services/presupuesto.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-listar-gasto',
   templateUrl: './listar-gasto.component.html',
   styleUrls: ['./listar-gasto.component.css']
 })
-export class ListarGastoComponent implements OnInit {
+export class ListarGastoComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+  presupuesto: number;
+  restante: number;
 
   constructor(
     private _presupuestoService: PresupuestoService) {
-    this._presupuestoService.getGastos().subscribe(data => {
+    this.presupuesto = 0;
+    this.restante = 0;
+    this.subscription = this._presupuestoService.getGastos().subscribe(data => {
       console.log(data);
     });
   }
 
   ngOnInit(): void {
+    this.presupuesto = this._presupuestoService.presupuesto;
+    this.subscription = this._presupuestoService.getGastos().subscribe(data => {
+      console.log(data);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
