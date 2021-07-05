fa016fb8cad5d2ca83b948a6d67e8ba0913a8358
Tiago Yamamoto <japa@rede5.com.br>
2021-07-05 01:13:11 -0300

    91. Ingresar Gasto - Bind Datos - Validaciones

--- src/app/components/gastos/ingresar-gasto/ingresar-gasto.component.ts
+++ src/app/components/gastos/ingresar-gasto/ingresar-gasto.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ingresar-gasto.component.css']
 })
 export class IngresarGastoComponent implements OnInit {
+  nombreGasto: string;
+  cantidad: number;
+  formularioIncorrecto: boolean;
+  textIncorrecto: string;
 
-  constructor() { }
+  constructor() {
+    this.nombreGasto = '';
+    this.cantidad = 0;
+    this.formularioIncorrecto = false;
+    this.textIncorrecto = 'Nombre gasto o cantidad incorrecta'
+  }
 
   ngOnInit(): void {
+  }
+
+  agregarGasto() {
+   if(this.nombreGasto === '' || this.cantidad <= 0){
+     this.formularioIncorrecto = true;
+   } else {
+     this.formularioIncorrecto = false;
+     this.nombreGasto = '';
+     this.cantidad = 0;
+   }
   }
 
 }
